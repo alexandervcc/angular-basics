b1b2e875b9140d2cef0c88b9c2c04c85721bd068
Alex Charco Cuenca <alex.charco.25@gmail.com>
2022-09-22 22:34:27 -0500

    ViewChild usage

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -15,4 +15,8 @@ export class ShoppingListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onIngredientCreated(newIngredient:Ingredient){
+    this.ingredients.push(newIngredient)
+  }
 }
