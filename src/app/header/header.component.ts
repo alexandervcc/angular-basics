829fb9c0790387af9e9520c1b3589263922c5923
Alex Charco Cuenca <alex.charco.25@gmail.com>
2022-09-28 07:32:47 -0500

    Header links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,12 +6,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  @Output('feature')
-  featuredSelected = new EventEmitter<string>();
-
   constructor() {}
-
-  onSelect(feature: string) {
-    this.featuredSelected.emit(feature);
-  }
 }
