a8ef51cf6b2e4f55a04db3fb9925e2bdebad10e6
Alexander Cuenca <alex.charco.25@gmail.com>
2022-10-16 12:32:18 -0500

    KeyFrames

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import {
   animate,
+  keyframes,
   state,
   style,
   transition,
@@ -89,6 +90,51 @@ import {
         ),
       ]),
     ]),
+    trigger("list2", [
+      state(
+        "in",
+        style({
+          opacity: 1,
+          transform: "translateX(0)",
+        })
+      ),
+      transition("void => *", [
+        animate(
+          1000,
+          keyframes([
+            style({
+              transform: "translateX(-100px)",
+              opacity: 0,
+              offset: 0,
+            }),
+            style({
+              transform: "translateX(-50px)",
+              opacity: 0.5,
+              offset: 0.3,
+            }),
+            style({
+              transform: "translateX(-20px)",
+              opacity: 1,
+              offset: 0.8,
+            }),
+            style({
+              transform: "translateX(0px)",
+              opacity: 1,
+              offset: 1,
+            }),
+          ])
+        ),
+      ]),
+      transition("* => void", [
+        animate(
+          300,
+          style({
+            transform: "translateX(100px)",
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
   ],
 })
 export class AppComponent {
@@ -140,4 +186,6 @@ export class AppComponent {
     - so use it when the DOM doesnt ahve the element, and you use an animation
       into adding the element in the DOM
 
+  -keyframes(): allow to be more precise of styles to occur in some timing
+    - to set the time for each phase, use 'offset' as css property
 */
