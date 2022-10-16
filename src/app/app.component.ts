22c2bcfea48039f5da68e7fff9151ff78c9a4d52
Alexander Cuenca <alex.charco.25@gmail.com>
2022-10-16 12:07:54 -0500

    Multiple animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,18 +25,50 @@ import {
           transform: "translateX(100px)",
         })
       ),
+      transition("normal <=> highlighted", animate(300)),
+    ]),
+    trigger("wildState", [
+      state(
+        "normal",
+        style({
+          "background-color": "red",
+          transform: "translateX(0) scale(1)",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px) scale(1)",
+        })
+      ),
+      state(
+        "shrunken",
+        style({
+          backgroundColor: "green",
+          transform: "translateX(0) scale(0.5)",
+        })
+      ),
       transition("normal => highlighted", animate(300)),
-      transition("highlighted => normal", animate(300)),
+      transition("highlighted => normal", animate(800)),
+      transition("shrunken <=> *", animate(500)),
+      
     ]),
   ],
 })
 export class AppComponent {
   state = "normal";
+  wildState = "normal";
   list = ["Milk", "Sugar", "Bread"];
 
   onAnimate() {
     console.log("preseed animation");
     this.state = this.state === "normal" ? "highlighted" : "normal";
+    this.wildState = this.wildState === "normal" ? "highlighted" : "normal";
+  }
+
+  onShrink() {
+    this.wildState = "shrunken";
   }
 
   onAdd(item) {
@@ -59,4 +91,7 @@ export class AppComponent {
           - string of transition: 'st1 => st2'
           - animate()
             - receives a number of ms for the change to occur
+  
+  - to add a transition bidirectiional use <=>
+  - to match any state use a wildcard: *
 */
